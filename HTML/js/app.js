e7f222e0c83e44164131a923fed16790018d339f
Naveen Nirmalaraj <naveen.nirmalaraj1995@gmail.com>
2018-03-14 05:31:17 -0400

    Prototype Code Finalized
    This is what we're demoing to Tung on Mar 14, 2018...

--- HTML/js/app.js
+++ HTML/js/app.js
@@ -1,4 +1,10 @@
 jQuery(function($){
+	var isChanged = false;
+	var prevGestureInt = 0;
+	var currentGesture = 0;
+
+	$('.theApp').hide();
+
 	poll = function(){
 		$.ajax({
 		url: "../Kinect/testVariables.txt",
@@ -6,8 +12,17 @@ jQuery(function($){
 		type: 'get',
 		cache:false,
 		success: function (data){
-			$("#output").text(data);
-			$("#output").text('blah');
+			var kinectInt = data.split(" ");
+			var gesture = kinectInt[0];
+			kinectInt = parseInt(kinectInt[1]);
+
+			if (kinectInt > prevGestureInt){
+				isChanged = true;
+				prevGestureInt = kinectInt;
+			
+				currentGesture = gesture; 
+
+			}
 		},
 		error: function(){
 			console.log('Error');
@@ -16,6 +31,51 @@ jQuery(function($){
 	},	
 	pollInterval = setInterval(function(){
 		poll();
-	}, 5000);
+
+		if(isChanged){
+			var appHovered = $(".selected").attr("id");
+
+			if (currentGesture == "R"){
+				$("#"+ appHovered).removeClass("selected");
+				appHovered = parseInt(appHovered);
+				var nextApp = 0;
+
+				if ( appHovered == 5 ){
+					nextApp = 1;
+				} else {
+					nextApp = appHovered + 1;
+				}
+
+				$("#" + String(nextApp)).addClass("selected");
+			}
+
+			if (currentGesture == "L"){
+				$("#"+ appHovered).removeClass("selected");
+				appHovered = parseInt(appHovered);
+				var prevApp = 0;
+
+				if ( appHovered == 1 ){
+					prevApp = 5;
+				} else {
+					prevApp = appHovered - 1;
+				}
+
+				$("#" + String(prevApp)).addClass("selected");
+			}
+
+			if (currentGesture == "P"){
+				$(".theApp").show();
+				$(".mainMenu").hide();
+			}
+
+			if (currentGesture == "B"){
+				$('.theApp').hide();
+				$(".mainMenu").show();
+			}
+
+			isChanged = false;
+		}
+
+	}, 1000);
 	poll();
 });
